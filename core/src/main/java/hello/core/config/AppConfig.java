525eb4eb13a0bfd3ec8e3154851e3ba5b75c8fb2
unknown <corojoon93@naver.com>
2020-12-07 01:13:01 +0900

    Refact AppConfig

--- core/src/main/java/hello/core/config/AppConfig.java
+++ core/src/main/java/hello/core/config/AppConfig.java
@@ -1,6 +1,9 @@
 package hello.core.config;
 
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -8,11 +11,19 @@ import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new RateDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), new RateDiscountPolicy());
     }
 }
