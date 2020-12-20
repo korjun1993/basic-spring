3a83f53814282915aae99b44685e0bf42a4cea3d
unknown <corojoon93@naver.com>
2020-12-20 15:40:31 +0900

    Apply AutoAppConfig using ComponentScan

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,13 +1,18 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
 
    // App Config 적용 전
    // private final MemberRepository memberRepository = new MemoryMemberRepository();
 
    // App Config 적용 후 -> DIP 충족 (interface 에만 의존)
-   private final MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
 
+    @Autowired // ac.getBean(MemberRepository.class)
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
