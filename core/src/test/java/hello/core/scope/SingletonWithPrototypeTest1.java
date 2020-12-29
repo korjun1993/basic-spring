a4e88e58188c4ae2099c07c5825de10a2d47b3e2
unknown <corojoon93@naver.com>
2020-12-29 23:55:42 +0900

    Add ObjectProvider of spring

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,6 +2,7 @@ package hello.core.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -34,20 +35,24 @@ public class  SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean{
-        private final PrototypeBean prototypeBean;
+//        private final PrototypeBean prototypeBean;
 
-        @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-            System.out.println("ClientBean.Constructor");
-        }
+        @Autowired // Bean으로 만들지 않아도 등록되어있음
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+
+//        @Autowired
+//        public ClientBean(PrototypeBean prototypeBean) {
+//            this.prototypeBean = prototypeBean;
+//            System.out.println("ClientBean.Constructor");
+//        }
 
         public int logic(){
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject(); // 스프링 컨테이너에서 해당 빈을 찾아서 반환해준다. Dependency Lookup
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
